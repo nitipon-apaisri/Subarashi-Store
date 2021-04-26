b5d89dc9192709ffec975a9eb921e127244644c9
nitipon apaisri <nitipon.apaisri@gmail.com>
2021-04-26 17:08:20 +0200

    Vue views Veew

---
+++ frontend/src/views/Home.vue
@@ -0,0 +1,38 @@
+<template>
+  <div class="home">
+    <h1>Register</h1>
+    <form @submit.prevent="register">
+      <input type="text" v-model="user.username" />
+      <input type="password" v-model="user.password" />
+      <button>Sign up</button>
+    </form>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+
+import axios from "axios";
+export default {
+  data() {
+    return {
+      user: {
+        username: "",
+        password: "",
+      },
+    };
+  },
+  methods: {
+    register() {
+      axios
+        .post("http://localhost:4200/register", {
+          username: this.user.username,
+          password: this.user.password,
+        })
+        .then((res) => {
+          console.log(res);
+        });
+    },
+  },
+};
+</script>
